7d3b27193d87b2ed14401ecaf0fd1e58ce28feb1
Pastalavisto <96241542+Pastalavisto@users.noreply.github.com>
2024-12-05 21:19:46 +0100

    feat : New route

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,11 @@ const routes = [
     name: 'Home',
     component: () => import('@views/Home.vue'),
   },
+  {
+    path: '/pokemon',
+    name: 'Pokemon',
+    component: () => import('@views/Pokemon.vue'),
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/Pokemon.vue
@@ -0,0 +1,31 @@
+<script>
+
+export default {
+  name: "Pokemon",
+  mixins: [],
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<template>
+<div>
+  <h1>Pokemon</h1>
+</div>
+</template>
+
+<style scoped>
+
+</style>
